e1dcd72ea3edd31211694c9212de7f39517ffda3
denis-beuermann <67262563+denis-beuermann@users.noreply.github.com>
2025-10-07 15:27:31 +0200

    Update pwshModuleInstaller.ps1

--- pwshModuleInstaller.ps1
+++ pwshModuleInstaller.ps1
@@ -1,6 +1,7 @@
 Write-Host 'Installing NuGet Package Provider...'
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Write-Host 'Installing Azure PowerShell module...'
-install-module az.accounts -Confirm:$false -AllowClobber -Force -verbose
+install-module az -Confirm:$false -AllowClobber -Force -verbose
 Write-Host 'Finishing Azure PowerShell module installation'
+
 
