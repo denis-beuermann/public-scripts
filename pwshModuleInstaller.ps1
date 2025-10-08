a9594a8b35a5ce1b7dd7b3b322b7033e1b69031b
denis-beuermann <67262563+denis-beuermann@users.noreply.github.com>
2025-10-08 15:03:19 +0200

    Update pwshModuleInstaller.ps1

--- pwshModuleInstaller.ps1
+++ pwshModuleInstaller.ps1
@@ -2,9 +2,12 @@ Write-Host 'Installing NuGet Package Provider...'
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Write-Host 'Installing Azure PowerShell module...'
 install-module az -Confirm:$false -AllowClobber -Force -verbose
+Write-Host 'Finishing Azure PowerShell module installation'
 Write-Host 'Installing Microsoft Graph module...'
 install-module Microsoft.Graph -Confirm:$false -AllowClobber -Force -verbose
-Write-Host 'Finishing Azure PowerShell module installation'
+Write-Host 'Finishing Microsoft Graph module installation'
+
+
 
 
 
